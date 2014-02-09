f8d592548d16e8d5c71c94bac2c28e23d9d5b4b3
CawaKharkov <cawa123@mail.ru>
2014-02-09 15:04:07 +0200

    license update

--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
 <?php
 // set up autoloader
 require ('./../vendor/autoload.php');
+
+//Сменим директорию нашего приложения
+chdir(dirname(__DIR__));
+//Константа домашней директории приложения, для удоства работы с путями
+define('APP_DIR', dirname(__DIR__));
